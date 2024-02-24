1670c19ccef2eb1efa5337986acbd5772ba7f262
Aditya Saligrama <aditya@saligrama.io>
2024-02-24 13:44:28 -0800

    Add remaining sites

--- sites.tf
+++ sites.tf
@@ -20,3 +20,72 @@ resource "cloudflare_pages_project" "saligrama" {
     root_dir        = ""
   }
 }
+
+resource "cloudflare_pages_project" "saligrama-blog" {
+  account_id        = var.cloudflare_account_id
+  name              = "saligrama-blog"
+  production_branch = "main"
+
+  source {
+    type = "github"
+    config {
+      owner                         = "saligrama"
+      repo_name                     = "blog"
+      production_branch             = "main"
+      deployments_enabled           = true
+      production_deployment_enabled = true
+    }
+  }
+
+  build_config {
+    build_command   = "hugo --gc --minify"
+    destination_dir = "public"
+    root_dir        = ""
+  }
+}
+
+resource "cloudflare_pages_project" "saligrama-notes" {
+  account_id        = var.cloudflare_account_id
+  name              = "saligrama-notes"
+  production_branch = "hugo-dev"
+
+  source {
+    type = "github"
+    config {
+      owner                         = "saligrama"
+      repo_name                     = "notes"
+      production_branch             = "hugo-dev"
+      deployments_enabled           = true
+      production_deployment_enabled = true
+    }
+  }
+
+  build_config {
+    build_command   = "hugo --gc --minify"
+    destination_dir = "public"
+    root_dir        = ""
+  }
+}
+
+resource "cloudflare_pages_project" "saligrama-photo" {
+  account_id        = var.cloudflare_account_id
+  name              = "saligrama-photo"
+  production_branch = "main"
+
+  source {
+    type = "github"
+    config {
+      owner                         = "saligrama"
+      repo_name                     = "notes"
+      production_branch             = "hugo-dev"
+      deployments_enabled           = true
+      production_deployment_enabled = true
+    }
+  }
+
+  build_config {
+    build_command   = "hugo --gc --minify"
+    destination_dir = "public"
+    root_dir        = ""
+  }
+}
