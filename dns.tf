c2aac48ce4967cd2b59691f0bf5eeca0035921ba
Aditya Saligrama <aditya@saligrama.io>
2024-02-24 16:54:22 -0800

    Import DNS records from Namecheap

--- dns.tf
+++ dns.tf
@@ -2,3 +2,120 @@ resource "cloudflare_zone" "saligrama_io" {
   account_id = var.cloudflare_account_id
   zone       = "saligrama.io"
 }
+
+resource "cloudflare_record" "root_saligrama_io_gh_pages_108" {
+  zone_id = cloudflare_zone.saligrama_io.id
+  type    = "A"
+  name    = "@"
+  value   = "185.199.108.153"
+}
+
+resource "cloudflare_record" "root_saligrama_io_gh_pages_109" {
+  zone_id = cloudflare_zone.saligrama_io.id
+  type    = "A"
+  name    = "@"
+  value   = "185.199.109.153"
+}
+
+resource "cloudflare_record" "root_saligrama_io_gh_pages_110" {
+  zone_id = cloudflare_zone.saligrama_io.id
+  type    = "A"
+  name    = "@"
+  value   = "185.199.110.153"
+}
+
+resource "cloudflare_record" "root_saligrama_io_gh_pages_111" {
+  zone_id = cloudflare_zone.saligrama_io.id
+  type    = "A"
+  name    = "@"
+  value   = "185.199.110.153"
+}
+
+resource "cloudflare_record" "_github_pages_challenge_saligrama_saligrama_io" {
+  zone_id = cloudflare_zone.saligrama_io.id
+  type    = "TXT"
+  name    = "_github-pages-challenge-saligrama"
+  value   = "ceaf204cce3688093e4b5481600300"
+}
+
+resource "cloudflare_record" "www_saligrama_io_gh_pages_cname" {
+  zone_id = cloudflare_zone.saligrama_io.id
+  type    = "CNAME"
+  name    = "www"
+  value   = "saligrama.github.io"
+}
+
+resource "cloudflare_record" "catshare_saligrama_io" {
+  zone_id = cloudflare_zone.saligrama_io.id
+  type    = "A"
+  name    = "catshare"
+  value   = "147.182.238.196"
+}
+
+resource "cloudflare_record" "root_saligrama_io_google_site_verification" {
+  zone_id = cloudflare_zone.saligrama_io.id
+  type    = "TXT"
+  name    = "@"
+  value   = "google-site-verification=Sd6tqhtWevi6UZXQuZKh-XyAYRQFgdLa1L0XQSyqST8"
+}
+
+resource "cloudflare_record" "root_saligrama_io_spf" {
+  zone_id = cloudflare_zone.saligrama_io.id
+  type    = "TXT"
+  name    = "@"
+  value   = "v=spf1 include:_spf.google.com ~all"
+}
+
+resource "cloudflare_record" "_dmarc_saligrama_io" {
+  zone_id = cloudflare_zone.saligrama_io.id
+  type    = "TXT"
+  name    = "_dmarc"
+  value   = "v=DMARC1; p=none; rua=mailto:dmarc@saligrama.io"
+}
+
+resource "cloudflare_record" "google_domainkey_saligrama_io" {
+  zone_id = cloudflare_zone.saligrama_io.id
+  type    = "TXT"
+  name    = "google._domainkey"
+  value   = "v=DKIM1; k=rsa; p=MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAhlCYGbwJJMAtyE6Fz6mEQDwVL9B3fEZSSfgQ9RrdBdOwpqfr8Ui3ZA6yeWSUE06p0/AfGgqYiTNF91P0BflwQb3IdsY+PggFwWzr7I+ZJIgKcfPuPEk+62DuuU+HIf9y+X+jl5IEQvsbEEqEcainzCXQz/N1uCL80kYev9h+UETMaBIayVdDWQ7Iw/4HUely3mZHh/y4Af66Ggv/Gs2EBUAH3jjj9HRL/a4mOe9p6DOigkkemMFe+gCjWUK9tjTifQSOR/WC26fzDRHBbkOJc6YWdJfxNIM+4qHSxZYzRuThYfk/+CMtwaoJdQvBwG9pp+w0W7zv+zkmyfzey5bEFQIDAQAB"
+}
+
+resource "cloudflare_record" "mx_aspmx_google_saligrama_io" {
+  zone_id  = cloudflare_zone.saligrama_io.id
+  name     = "@"
+  value    = "aspmx.l.google.com"
+  type     = "MX"
+  priority = 1
+}
+
+resource "cloudflare_record" "mx_alt1_google_saligrama_io" {
+  zone_id  = cloudflare_zone.saligrama_io.id
+  name     = "@"
+  value    = "alt1.aspmx.l.google.com"
+  type     = "MX"
+  priority = 5
+}
+
+resource "cloudflare_record" "mx_alt_google_saligrama_io" {
+  zone_id  = cloudflare_zone.saligrama_io.id
+  name     = "@"
+  value    = "alt2.aspmx.l.google.com"
+  type     = "MX"
+  priority = 5
+}
+
+resource "cloudflare_record" "mx_alt3_google_saligrama_io" {
+  zone_id  = cloudflare_zone.saligrama_io.id
+  name     = "@"
+  value    = "alt3.aspmx.l.google.com"
+  type     = "MX"
+  priority = 10
+}
+
+resource "cloudflare_record" "mx_alt4_google_saligrama_io" {
+  zone_id  = cloudflare_zone.saligrama_io.id
+  name     = "@"
+  value    = "alt4.aspmx.l.google.com"
+  type     = "MX"
+  priority = 10
+}
