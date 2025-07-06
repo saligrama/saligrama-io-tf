ac77a290d73896490e9cd168d564795b41a47810
Aditya Saligrama <aditya@saligrama.io>
2025-07-05 23:27:13 -0700

    scaffold transitdiary

--- sites.tf
+++ sites.tf
@@ -139,3 +139,25 @@ resource "cloudflare_pages_domain" "catshare" {
   project_name = cloudflare_pages_project.catshare.name
   domain       = "catshare.saligrama.io"
 }
+
+resource "cloudflare_pages_project" "transitdiary" {
+  account_id        = var.cloudflare_account_id
+  name              = "transitdiary"
+  production_branch = "main"
+
+  source {
+    type = "github"
+    config {
+      owner                         = "saligrama"
+      repo_name                     = "transitdiary"
+      production_branch             = "main"
+      deployments_enabled           = true
+      production_deployment_enabled = true
+    }
+  }
+}
+
+resource "cloudflare_d1_database" "transitdiary_db" {
+  account_id = var.cloudflare_account_id
+  name       = "transitdiary"
+}
